59bd952eaaf80ed74c494af25ff0888986ed18b0
Sergey Krivosheev <fenix_ex@mail.ru>
2019-10-01 13:56:36 +0300

    without config

--- traits/LogsTrait.php
+++ traits/LogsTrait.php
@@ -9,15 +9,19 @@ trait LogsTrait
 
     public function beforeAction($action)
     {
-        if (Yii::$app->getModule('logs') && Yii::$app->getModule('logs')->enabled) {
-            $log = new ActionLogs();
-            $log->url = Yii::$app->request->url;
-            $log->post_data = json_encode(Yii::$app->request->post());
-            $log->id_user = Yii::$app->user->id;
-            $log->controller = $action->controller->className();
-            $log->action = $action->actionMethod;
-            $log->referer = Yii::$app->request->referrer;
-            $log->save();
+        try {
+            if (Yii::$app->getModule('logs') && Yii::$app->getModule('logs')->enabled) {
+                $log = new ActionLogs();
+                $log->url = Yii::$app->request->url;
+                $log->post_data = json_encode(Yii::$app->request->post());
+                $log->id_user = Yii::$app->user->id;
+                $log->controller = $action->controller->className();
+                $log->action = $action->actionMethod;
+                $log->referer = Yii::$app->request->referrer;
+                $log->save();
+            }
+        } catch (\Exception $e) {
+
         }
         return parent::beforeAction($action);
     }
